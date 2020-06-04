7cec04e9e7243b1956a82ecd59ada3b99bed4a37
anibal gomez <anibalgomez@MacBook-Pro-de-anibal.local>
2020-06-04 17:27:28 -0500

    bar themes

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bar',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class BarComponent implements OnInit {
 
   constructor() { }
+
+  @Input() theme;
 
   barPercent = 0;
   barFill = 1;
